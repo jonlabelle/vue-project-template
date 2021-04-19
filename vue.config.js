99fd9f97a2e2b8d78ee62d033eb58d2f6e875893
Jon LaBelle <contact@jonlabelle.com>
2021-04-19 02:45:37 -0500

    Enable runtimeCompiler

--- vue.config.js
+++ vue.config.js
@@ -9,10 +9,17 @@ module.exports = {
 
   // Setting this to false can speed up production builds
   // if you don't need source maps for production.
-  productionSourceMap: false
+  productionSourceMap: false,
 
   // Set to true to enable Subresource Integrity (SRI).
   // If you are hosting your built files on a CDN, it
   // is a good idea to enable this for additional security.
-  // integrity: true,
+  integrity: false,
+
+  // Whether to use the build of Vue core that includes
+  // the runtime compiler. Setting it to true will allow
+  // you to use the template option in Vue components,
+  // but will incur around an extra 10kb payload for
+  // your app.
+  runtimeCompiler: true
 };
