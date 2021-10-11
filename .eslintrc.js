da00544d7affad0e14330ce525fdf357c084f403
Jon LaBelle <contact@jonlabelle.com>
2021-10-11 07:37:59 -0500

    Update @vue/cli-service to 5.0.0-beta.5
    - Use Jest instead of Mocha/Chai

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,17 @@
 module.exports = {
   root: true,
-
   env: {
     node: true
   },
-
+  extends: ["plugin:vue/recommended", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "@babel/eslint-parser"
   },
-
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "prettier/prettier": "error"
   },
-
   overrides: [
     {
       files: [
@@ -22,10 +19,8 @@ module.exports = {
         "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
-        mocha: true
+        jest: true
       }
     }
-  ],
-
-  extends: ["plugin:vue/recommended", "eslint:recommended", "@vue/prettier"]
+  ]
 };
