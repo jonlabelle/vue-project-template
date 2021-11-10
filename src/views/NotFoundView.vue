6e4a2abe0ac3a4cdcc7e4862073c286eb59dd658
Jon LaBelle <contact@jonlabelle.com>
2021-11-10 00:41:00 -0600

    Update to vue-cli ~5.0.0-beta.7

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="notfound">
+    <h1>Oops!</h1>
+    <h3 class="lead">The {{ resource }} you're looking for is not here.</h3>
+    <router-link :to="{ name: 'home' }">Back to the home page</router-link>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      resource: {
+        type: String,
+        required: false,
+        default: "resource"
+      }
+    }
+  };
+</script>
