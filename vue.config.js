713db820d43374479d9025f7ffc0faeb8cce070d
Jon LaBelle <contact@jonlabelle.com>
2021-11-10 01:30:27 -0600

    Doc block updates

--- vue.config.js
+++ vue.config.js
@@ -1,72 +1,18 @@
 const { defineConfig } = require("@vue/cli-service");
 
-//const path = require("path");
-
 /**
  * @type {import('@vue/cli-service').ProjectOptions}
  * @see https://next.cli.vuejs.org/config/
  */
 module.exports = defineConfig({
-  //
-  // By default babel-loader ignores all files inside node_modules. You can
-  // enable this option to avoid unexpected untranspiled code from third-party
-  // dependencies.
-  //
-  // Transpiling all the dependencies could slow down the build process, though.
-  // If build performance is a concern, you can explicitly transpile only some
-  // of the dependenciesby passing an array of package names or name patterns
-  // to this option.
-  //
-  // Default: false
   transpileDependencies: true,
-
-  //
-  // A directory (relative to outputDir) to nest generated static assets
-  // (js, css, img, fonts) under.
-  //
-  // Tip: `assetsDir` is ignored when overwriting the filename or
-  // chunkFilename from the generated assets.
-  //
-  // Default: ''
   assetsDir: "assets",
-
-  //
-  // Setting this to false can speed up production builds
-  // if you don't need source maps for production.
-  //
-  // Default: true
   productionSourceMap: false,
 
-  //
-  // Set to true to enable Subresource Integrity (opens new window) (SRI) on
-  // <link rel="stylesheet"> and <script> tags in generated HTML. If you are
-  // hosting your built files on a CDN, it is a good idea to enable this for
-  // additional security.
-  //
-  // Note that this only affects tags injected by html-webpack-plugin - tags
-  // directly added in the source template (public/index.html) are not
-  // affected.
-  //
-  // Also, when SRI is enabled, preload resource hints are disabled due to a
-  // bug in Chrome (opens new window) which causes the resources to be
-  // downloaded twice.
-  //
-  // Default: false
-  integrity: false,
-
-  //
-  // Whether to use the build of Vue core that includes the runtime compiler.
-  // Setting it to true will allow you to use the template option in
-  // Vue components, but will incur around an extra 10kb payload for your app.
-  //
-  // Default: false
-  runtimeCompiler: false,
-
+  /**
+   * @type {import('@vue/cli-service').ProjectOptions.css}
+   */
   css: {
-    //
-    // Whether to enable source maps for CSS.
-    // Setting this to true may affect build performance.
-    // Default: false
     sourceMap: false
 
     //
